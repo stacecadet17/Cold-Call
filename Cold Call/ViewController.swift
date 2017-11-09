dfe781ccfbc8d3636c95364098c08da2d8e5ccae
stacecadet17 <staceyabbott@stacey-abbotts-macbook.local>
2017-11-09 13:12:14 -0800

    first commit

--- Cold Call/ViewController.swift
+++ Cold Call/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    let people = ["Leslie", "Ron", "Jerry", "Tom", "Donna", "Anne", "April", "Andy", "Ben", "Chris"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func changeName(_ sender: UIButton) {
+        
+        let personNumber = Int(arc4random_uniform(UInt32(people.count)))
+            nameLabel.text = people[personNumber]
     }
-
+    
 
 }
 
